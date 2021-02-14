333c1737b528dc7231c24fca66d31ebd401dfded
Indira Rojas <inroes.soluciones@gmail.com>
2021-02-14 12:46:28 -0500

    feat(2a) - Crear componentes de tipo contenedor/fetch  como CourseContainer y InstructorContainer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CourseContainerComponent } from './components/course-container/course-container.component';
+import { InstructorContainerComponent } from './components/instructor-container/instructor-container.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CourseContainerComponent, InstructorContainerComponent],
   imports: [
     CommonModule
   ]
